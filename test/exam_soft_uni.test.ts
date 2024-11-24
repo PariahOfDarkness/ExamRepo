a26eb43fc9aa04cb3d30e6356d07f23f18b7bbd6
PariahOfDarkness <pisimau@gmail.com>
2024-11-24 12:34:36 +0200

    Initial commit

---
+++ test/exam_soft_uni.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExamSoftUni from '../lib/exam_soft_uni-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/exam_soft_uni-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExamSoftUni.ExamSoftUniStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
